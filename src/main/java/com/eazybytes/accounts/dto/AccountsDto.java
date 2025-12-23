799a392075c0bfa197fa30a626ce07765c86489a
Sheetal Kabra <sheetalkabra4@gmail.com>
2025-12-23 17:32:14 +0530

    created Entities and Repositories for Accounts, Customer, created DTo;s

---
+++ src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class AccountsDto {
+    private Long accountNumber;
+    private String accountType;
+    private String branchAddress;
+}
--- src/main/java/com/eazybytes/accounts/entity/Customer.java
+++ src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -1,2 +1,20 @@
-package com.eazybytes.accounts.entity;public class Customer {
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    private String name;
+    private String email;
+    private String mobileNumber;
+
 }
