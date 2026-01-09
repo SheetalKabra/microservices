2ff50f39c73513c76e648466f1786064e6a696bb
Sheetal Kabra <sheetalkabra4@gmail.com>
2026-01-09 11:19:34 +0530

    implementing the third way to read the properties from spring cloud config which is github repo, now using refresh api

--- section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -1,13 +1,17 @@
 package com.eazybytes.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-    //makes these fields final,
-    // and create only getter methods,
-    // no setter methods
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
