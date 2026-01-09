34f262c2574a9cba443fdbc98b5aa947b184b15e
Sheetal Kabra <sheetalkabra4@gmail.com>
2026-01-09 17:50:34 +0530

    remove h2 db and add mysql dependency in pom.xml and same in application.yml

--- section7/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ section7/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
         description = "Schema to hold failed response information"
 )
 @Data
-@AllArgsConstructor
 public class ErrorResponseDto {
     @Schema(
             description = "Api path invoked by client"
@@ -30,4 +29,43 @@ public class ErrorResponseDto {
             description = "The time when error happened"
     )
     private LocalDateTime errorTime;
+
+    public ErrorResponseDto(String apiPath, HttpStatus errorCode, String errorMessage, LocalDateTime errorTime) {
+        this.apiPath = apiPath;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.errorTime = errorTime;
+    }
+
+    public String getApiPath() {
+        return apiPath;
+    }
+
+    public void setApiPath(String apiPath) {
+        this.apiPath = apiPath;
+    }
+
+    public HttpStatus getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(HttpStatus errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public LocalDateTime getErrorTime() {
+        return errorTime;
+    }
+
+    public void setErrorTime(LocalDateTime errorTime) {
+        this.errorTime = errorTime;
+    }
 }
